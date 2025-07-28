b75fc2aa9df039a4699e9785b95a6c244afe3b04
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2025-07-28 13:40:09 +0300

    tasksApi_RTK_Query

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx
@@ -1,26 +1,26 @@
-import { useAppDispatch } from "@/common/hooks"
-import { createTaskTC } from "@/features/todolists/model/tasks-slice"
 import type { DomainTodolist } from "@/features/todolists/model/todolists-slice"
 import { FilterButtons } from "./FilterButtons/FilterButtons"
 import { Tasks } from "./Tasks/Tasks"
 import { TodolistTitle } from "./TodolistTitle/TodolistTitle"
 import { CreateItemForm } from "@/common/components/CreateItemForm/CreateItemForm"
+import { useCreateTaskMutation } from "@/features/todolists/api/tasksApi"
 
 type Props = {
   todolist: DomainTodolist
 }
 
 export const TodolistItem = ({ todolist }: Props) => {
-  const dispatch = useAppDispatch()
 
-  const createTask = (title: string) => {
-    dispatch(createTaskTC({ todolistId: todolist.id, title }))
+  const [createTask] = useCreateTaskMutation()
+
+  const _createTask = (title: string) => {
+    createTask({ todolistId: todolist.id, title })
   }
 
   return (
     <div>
       <TodolistTitle todolist={todolist} />
-      <CreateItemForm onCreateItem={createTask} disabled={todolist.entityStatus === "loading"} />
+      <CreateItemForm onCreateItem={_createTask} disabled={todolist.entityStatus === "loading"} />
       <Tasks todolist={todolist} />
       <FilterButtons todolist={todolist} />
     </div>
