f69a38348deb026e9888dab786a509dbc9519ac9
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2025-08-13 22:31:58 +0300

    gh-pages_4

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-to-do-list/',
   build: {
-    outDir: 'build',
-    emptyOutDir: true,
+    outDir: 'dist',
+    emptyOutDir: true
   },
   resolve: {
     alias: {
